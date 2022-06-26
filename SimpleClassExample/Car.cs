27ee58ee4b4cfa71f5e97baf302d8267cdb64c89
Covali Andrei <krab4ek@gmail.com>
2022-06-26 20:09:04 +0300

    Add project files.

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleClassExample
+{
+    internal class Car
+    {
+        public string petName;
+        public int currSpeed;
+        public Car()
+        {
+            petName = "Chuck";
+            currSpeed = 10;
+        }
+        public Car(string pn) => petName = pn;
+
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+        public void PrintState() => Console.WriteLine($"{petName} is going {currSpeed}.");
+        public void SpeedUp(int delta) => currSpeed += delta;
+    }
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using SimpleClassExample;
+
+Console.WriteLine("***** Fun with Class types *****\n");
+Car myCar = new Car();
+myCar.petName = "Henry";
+myCar.currSpeed = 10;
+myCar.PrintState();
+//for (int i = 10; i >= 0; i--)
+//{
+//    myCar.SpeedUp(5);
+//    myCar.PrintState();
+//}
+
+Car chuck = new Car();
+chuck.PrintState();
+
+Car mary = new Car("Marry");
+mary.PrintState();
+
+Car daisy = new Car("Daisy", 75);
+daisy.PrintState();
+
+Console.ReadLine();
